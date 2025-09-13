4259d93e58970fd71175c002c15b48cdb65a23aa
Ashton <ashton.anderson.prentice@gmail.com>
2025-09-13 15:49:41 +0800

    Initial commit to repo

---
+++ wordle.py
@@ -0,0 +1,256 @@
+# ictprg302_AT3
+#
+# Author: Ashton_Anderson
+# Student ID: 20152010
+#
+# Course: ICTPRG302
+# Lecturer: Rafael Avigad
+
+# TODO: Add Import statements (if needed)
+import random
+
+# Variables and Constants
+# TODO: Define Constants & Variables
+DEBUG = False
+
+
+target_words = "target_words.txt"
+guess_words = "all_words.txt"
+
+
+
+
+
+
+
+# Application Functions
+def random_target_word(word_list):
+    word_list_length = len(word_list)
+
+    random_num = random.randint(0 , word_list_length -1)
+
+    return word_list[random_num]
+
+def display_score(score, guess_word):
+    score_output = ""
+    word_output = ""
+
+    for i in range (len(score)):
+        if score[i] == 0:
+            score_output += "-"
+        if score[i] == 1:
+            score_output += "?"
+        if score[i] == 2:
+            score_output += "X"
+        score_output += " "
+            
+    for i in range (len(score)):
+        word_output += guess_word[i]
+
+        word_output += " "
+
+    print(score_output)
+    print(word_output)
+            
+
+# TODO: Score Guess Function
+def score_guess(guess_word, target_word):
+    """Determines the score of a users guess compared to the target
+    Arguments
+    ---------
+    guess_word: The word that has been input by the user
+    target_word: The randomly selected word from target_words.txt that the user is
+    trying to guess
+
+    Returns
+    -------
+    score_list: This is the returned list that determines the users score. 0 for an
+    incorrect letter, 1 for a correct letter in the wrong position, 2 for a correct
+    letter in the correct position
+
+    Examples
+    --------
+    guess_word = hello
+    target_word = world
+    guess_score = 0,1,0,2,0
+
+    guess_word = hello
+    target_word = hello
+    guess_score = 2,2,2,2,2"""
+
+    target_list = list(target_word)
+    guess_list=list(guess_word)
+
+    score_list = [0] * len(target_list)
+
+
+    for i in range(len(target_list)):
+        if guess_list[i] == target_list[i]:
+            score_list[i] = 2
+            target_list[i] = None
+            
+    for i in range(len(target_list)):
+        if score_list[i] == 0 and guess_list[i] in target_list:
+            score_list[i] = 1
+            target_list[target_list.index(guess_list[i])] = None
+            
+
+    return score_list
+
+
+
+
+# TODO: Read File Into Word List Function
+def read_words_into_list(filename):
+    list_of_words = []
+
+    with open(filename, 'r') as f:
+        for line in f:
+            list_of_words.append(line.strip())
+        
+    return list_of_words
+
+# TODO: Display Greeting Function
+def show_greeting():
+    print("""
+Welcome to
++===================================================+
+|██╗0000██╗0██████╗0██████╗0██████╗0██╗00000███████╗|
+|██║0000██║██╔═══██╗██╔══██╗██╔══██╗██║00000██╔════╝|
+|██║0█╗0██║██║000██║██████╔╝██║00██║██║00000█████╗00|
+|██║███╗██║██║000██║██╔══██╗██║00██║██║00000██╔══╝00|
+|╚███╔███╔╝╚██████╔╝██║00██║██████╔╝███████╗███████╗|
+|0╚══╝╚══╝00╚═════╝0╚═╝00╚═╝╚═════╝0╚══════╝╚══════╝|
++===================================================+
+""")
+
+    player_name = input("Enter your name: ")
+    print(f"Hello, {player_name}! Let's play!")
+    return player_name
+
+
+
+# TODO: Display Instructions Function
+def show_instructions(player_name):
+    show_instruction = input(f"Do you need the instructions {player_name}? ")
+    if show_instruction.lower() in ('y', 'yes'):
+        print(r""""
++===============================================================+
+|  _                                        _                   |
+| (_)           _                      _   (_)                  |
+|  _ ____   ___| |_   ____ _   _  ____| |_  _  ___  ____   ___  |
+| | |  _ \ /___)  _) / ___) | | |/ ___)  _)| |/ _ \|  _ \ /___) |
+| | | | | |___ | |__| |   | |_| ( (___| |__| | |_| | | | |___ | |
+| |_|_| |_(___/ \___)_|    \____|\____)\___)_|\___/|_| |_(___/  |
++===============================================================+
+-You will be given 5 tries to guess a 5 letter word!
+-For each try your guess will be scored!
+-If a letter you guess is in the word and in the correct position you will get a X
+-If a letter you guess is in the word but in the incorrect position you will get a ?
+-If a letter you guess is not in the word you will get a -
+-For example:
+|------------------| |------------------------------|
+|Target: H e l l o | |- = Incorrect                 |
+| Guess: W o r l d | |? = Correct (Wrong Position)  |
+|Score: (- ? - X -)| |X = Correct (Correct Position)|
+|------------------| |------------------------------|
+If you do not guess the word in 5 tries it's game over!
+- Type --help at any time to see the instructions again.
+Good Luck!""")
+        input("Ready?")
+    else:
+        print("Okay!\n")
+
+# TODO: Any Optional Additional Functions
+def play_again(answer):
+    if answer.lower() in ("y" , "yes"):
+        return True
+    else:
+        return False
+    
+
+
+# TODO: Play Game Function
+def play_game():
+    play_again_bool = True
+    
+    player_name = show_greeting()
+    show_instructions(player_name)
+    while play_again_bool == True:
+        target_list = read_words_into_list(target_words)
+        target_word = random_target_word(target_list)
+
+        guess_list = read_words_into_list(guess_words)
+
+        max_attempts = 5
+        attempts = 0
+        
+
+        while attempts < max_attempts:
+            guess_word = input("Guess The Word: \n")
+
+            if len(guess_word)!= 5:
+                print("your guess must be 5 letters\n")
+                continue
+            if guess_word.lower() not in guess_list:
+                print("Invalid word. Try again\n")
+                continue
+            
+            if guess_word == "--help":
+                show_instructions(player_name)
+                
+
+        
+            attempts += 1
+
+            print(f"Attempt Number: {attempts}/5")
+            if attempts == 4:
+                print(f"Last attempt! Attempt Number: {attempts}/5")
+                
+            score_list = score_guess(guess_word, target_word)
+            display_score(score_list, guess_word)
+            print("\n")
+
+            if guess_word.lower() == target_word.lower():
+                print(f"You guessed the word!! {target_word}")
+                break
+
+        else:
+            print(f"""Unlucky, the word was
+      {target_word.upper()}""")
+            
+
+        answer = input("Play Again? y/n: \n")
+        play_again_bool = play_again(answer)
+        if play_again_bool == True:
+            print("Generating new word...")
+    print("Thanks, For Playing!")
+        
+        
+    
+    
+        
+        
+    
+
+def test_game():
+    # Test Case 2
+    ## Arrange
+    guess_word = "heelo"
+    target_word = "heelo"
+    guess_word = input ("Guess The Word: \n")
+    ## Act
+    score = score_guess(guess_word, target_word)
+    display_score(score, guess_word)
+
+    ## Assert
+    print("Score:", score, "Expected:", [2, 2, 2, 2, 2])
+     
+# Main Program
+if DEBUG:
+    test_game()
+else:
+    play_game()
+
+
+
